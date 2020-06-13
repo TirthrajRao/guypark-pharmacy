399be68f8601894e562a494348dee6240692c3bf
komalsakhiya <komalsakhiya21@gmail.com>
2020-06-13 14:52:39 +0530

    contact us page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { UserService } from '../services/user.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,8 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  language: string = "en";
+  details: any = this._translate.instant("contact")
+  contactForm: FormGroup;
+  formDetail: any = this._translate.instant("form")
+  submitted: Boolean = false;
+  isDisable:Boolean = false;
 
-  ngOnInit() {}
+  constructor(
+    private _translate: TranslateService,
+    public _userService: UserService
+  ) {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      subject: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required]),
+
+    })
+    this._initialiseTranslation();
+  }
+
+  ngOnInit() {
+    this._userService.languageChanges().subscribe((res: any) => {
+      console.log("RESPONSE", res);
+      this.language = res.language
+      this._initialiseTranslation();
+    })
+  }
+
+  get f() { return this.contactForm.controls }
+
+  /**
+   * Detect and change language
+   */
+  _initialiseTranslation(): void {
+    this._translate.use(this.language);
+    setTimeout(() => {
+      console.log(this._translate.instant("contact"));
+      this.details = this._translate.instant("contact");
+      this.formDetail = this._translate.instant("form")
+
+    }, 250);
+  }
+
+  /**
+   * Add Contact form
+   * @param {object} data 
+   */
+  addContactForm(data) {
+    this.submitted = true;
+    if (this.contactForm.invalid) {
+      return
+    }
+    this.isDisable = true;
+    console.log(data)
+  }
 
 }
